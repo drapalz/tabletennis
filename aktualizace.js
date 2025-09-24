e12fb5372c311f23c961db56fb07e22c36935aa2
drapalz <drapalz@email.cz>
2025-09-24 09:48:06 +0200

    Update aktualizace.js

--- aktualizace.js
+++ aktualizace.js
@@ -173,7 +173,7 @@ async function fetchUpcomingMatches(maxPages = 3) {
   return allMatches;
 }
 
-async function fetchAllMatches(maxPages = 1000, startPage = 250) {
+async function fetchAllMatches(maxPages = 1000, startPage = 555) {
   let allMatches = [];
   for (let page = startPage; page <= maxPages; page++) {
     const url = `https://api.b365api.com/v3/events/ended?sport_id=${SPORT_ID}&token=${TOKEN}&league_id=${LEAGUE_ID}&per_page=100&page=${page}`;
@@ -193,7 +193,7 @@ async function fetchAllMatches(maxPages = 1000, startPage = 250) {
 app.get('/matches', async (req, res) => {
   try {
     console.log('Fetching all matches...');
-    const matches = await fetchAllMatches(1000, 250);
+    const matches = await fetchAllMatches(1000, 555);
     console.log(`Načteno ${matches.length} zápasů, ukládám do DB...`);
     await upsertMatchesToDb(matches);
     console.log('Uložení zápasů do DB proběhlo.');
